c7e3e8b9c4eb1537f51ecbac907f03f19662c8c0
bomboseco <rioseco0805@gmail.com>
2023-11-25 18:16:36 -0300

    Models rest

--- gestion/rest/models.py
+++ gestion/rest/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class evento(models.Model):
+    fecha_inicio = models.DateField()
+    fecha_termino = models.DateField()
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    tipo_choices = ['Vacaciones', 'Feriado', 'Suspensión de actividades', 'Suspension de actividades pm', 
+            'Periodo Lectivo', 'Suspensión de evaluaciones', 'Ceremonia', 'EDDA', 'Evaluacion',
+            'Ayudantias', 'Hito académico', 'Secretaria académica', 'OAI']
+    tipo = models.CharField(max_length=100, choices=tipo_choices)
+    segmento = models.ManyToManyField('segmento')
+    def __str__(self):
+        return self.titulo
+    
+class segmento(models.Model):
+    nombre = models.CharField(max_length=13)
+    def __str__(self):
+        return self.nombre
+
+
