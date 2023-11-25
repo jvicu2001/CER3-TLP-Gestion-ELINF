0d2b747f33bda61a15dfabc25e7ef61d717d7949
bomboseco <rioseco0805@gmail.com>
2023-11-25 19:41:58 -0300

    Model fixed

--- gestion/rest/models.py
+++ gestion/rest/models.py
@@ -6,9 +6,9 @@ class evento(models.Model):
     fecha_termino = models.DateField()
     titulo = models.CharField(max_length=100)
     descripcion = models.TextField()
-    tipo_choices = ['Vacaciones', 'Feriado', 'Suspensión de actividades', 'Suspension de actividades pm', 
-            'Periodo Lectivo', 'Suspensión de evaluaciones', 'Ceremonia', 'EDDA', 'Evaluacion',
-            'Ayudantias', 'Hito académico', 'Secretaria académica', 'OAI']
+    tipo_choices = [('V','Vacaciones'), ('F','Feriado'), ('SA','Suspensión de actividades'), ('SAP','Suspension de actividades pm'), 
+            ('PL','Periodo Lectivo'), ('SE','Suspensión de evaluaciones'), ('C','Ceremonia'), ('E','EDDA'), ('EV','Evaluacion'),
+            ('Ay','Ayudantias'), ('HA','Hito académico'), ('SeA','Secretaria académica'), ('OAI','OAI')]
     tipo = models.CharField(max_length=100, choices=tipo_choices)
     segmento = models.ManyToManyField('segmento')
     def __str__(self):
