0e19de641abba2906fbbaba885751e788e5fadb6
bomboseco <rioseco0805@gmail.com>
2023-11-26 17:03:44 -0300

    admin rest

--- gestion/rest/admin.py
+++ gestion/rest/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from rest.models import evento, segmento
 
-# Register your models here.
+class eventoAdmin(admin.ModelAdmin):
+    model = evento
+    verbose_name_plural = 'Eventos'
+
+
+class segmentoAdmin(admin.ModelAdmin):
+    model = segmento
+    verbose_name_plural = 'Segmentos'
+
+admin.site.register(evento, eventoAdmin)
