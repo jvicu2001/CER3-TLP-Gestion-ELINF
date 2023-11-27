1d54895a9468ae7d5d66485352159416b89767d0
Jorge Vicuña <jvicuna2001@gmail.com>
2023-11-26 21:31:54 -0300

    Muestra de datos inicial

--- gestion/front/views.py
+++ gestion/front/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 
+from    rest.views import EventoList
+
 # Create your views here.
 def publicList(request: HttpRequest):
-    return render(request, 'listadoPublico.html')
+    # Obtener eventos
+    eventos = EventoList.as_view()(request)
+
+    return render(request, 'listadoPublico.html', {'eventos': eventos.data})
 
 def segmentList(request: HttpRequest):
     return render(request, 'listadoSegmento.html')
