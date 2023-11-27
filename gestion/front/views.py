fdcf8e36e953ef0e1f4a9818de9230171aacd9a2
Jorge Vicuña <jvicuna2001@gmail.com>
2023-11-27 19:17:24 -0300

    Implementados filtros búsqueda eventos

--- gestion/front/views.py
+++ gestion/front/views.py
@@ -1,14 +1,40 @@
+from typing import OrderedDict
 from django.shortcuts import render
 from django.http import HttpRequest
 
-from    rest.views import EventoList
+from rest.views import EventoList, SegmentoList
+from rest.models import evento as eventoModel
 
 # Create your views here.
 def publicList(request: HttpRequest):
     # Obtener eventos
-    eventos = EventoList.as_view()(request)
+    eventos: OrderedDict = EventoList.as_view()(request).data
+    segmentos: OrderedDict = SegmentoList.as_view()(request).data
 
-    return render(request, 'listadoPublico.html', {'eventos': eventos.data})
+    tipos = eventoModel.tipo_choices
+
+    # Filtrar eventos
+    tipo = request.GET.get('tipo')
+    segmento = request.GET.get('segmento')
+
+    if tipo:
+        eventos = list(filter(lambda x: x['tipo'] == tipo, eventos))   
+    
+    if segmento:
+        eventos = list(filter(
+            lambda eventos_a_filtrar: 
+                map(lambda segmentos_evento: 
+                    segmentos_evento['id'], eventos_a_filtrar['segmento']
+                ), eventos
+            ))
+
+    return render(request, 'listadoPublico.html', {
+        'eventos': eventos,
+        'segmentos': segmentos,
+        'segmento_actual': int(segmento) if segmento else None,
+        'tipo_actual': tipo,
+        'tipos': tipos
+        })
 
 def segmentList(request: HttpRequest):
     return render(request, 'listadoSegmento.html')
