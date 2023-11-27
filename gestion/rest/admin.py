dcc2201b29a4c2bab073958beb160969d9de4de1
Jorge Vicuña <jvicuna2001@gmail.com>
2023-11-27 20:37:32 -0300

    Cambiar modelo de usuario

--- gestion/rest/admin.py
+++ gestion/rest/admin.py
@@ -1,29 +1,17 @@
 from django.contrib import admin
-from rest.models import evento, segmento, AdminSegmento
+from rest.models import evento, segmento, UserSegmento
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.db.models.query import QuerySet
 from django.http.request import HttpRequest
 
-class segmento_inline(admin.TabularInline):
-    model = segmento
-    extra = 4
 
 class eventoAdmin(admin.ModelAdmin):
     model = evento
     verbose_name_plural = 'Eventos'
-
-class AdminSegmentoInline(admin.StackedInline):
-    model = AdminSegmento
-    can_delete = False
-    verbose_name_plural = 'AdminSegmentos'
     
-class UserAdmin(BaseUserAdmin):
-    inlines = (AdminSegmentoInline, )
 
-#Re-register userAdmin
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+#Re-register
 
 admin.site.register(evento, eventoAdmin)
 admin.site.register(segmento)
