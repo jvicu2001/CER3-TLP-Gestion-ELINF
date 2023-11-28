ceec04192149dd711f4fcba8ab7e749129ed264e
bomboseco <rioseco0805@gmail.com>
2023-11-28 01:18:04 -0300

    POST PUT DELETE Staff only enabled

--- gestion/rest/viewsets.py
+++ gestion/rest/viewsets.py
@@ -1,11 +1,15 @@
 from rest_framework import viewsets 
 from . import models
 from . import serializers
+from .permissions import IsAdminUserOrReadOnly
+
 
 class EventoViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminUserOrReadOnly]
     queryset = models.evento.objects.all()
     serializer_class = serializers.EventoSerializer
     
 class SegmentoViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAdminUserOrReadOnly]
     queryset = models.segmento.objects.all()
     serializer_class = serializers.SegmentoSerializer
